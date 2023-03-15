80c8e306e744d5b4378b4ad0315d1564375bd634
Rafael Castro <rafakastro27@gmail.com>
2023-03-14 21:57:25 -0500

    Ponemos en practica el useEffect de manera superficial

--- src/App/AppPrincipal.js
+++ src/App/AppPrincipal.js
@@ -1,51 +1,45 @@
-import React from 'react';
-import TodoCounter from '../TodoCounter';
-import TodoList from '../TodoList';
-import TodoItem from '../TodoItem';
-import TodoSearch from '../TodoSearch';
-import CreateTodoButton from '../CreateTodoButton';
-
+import React from "react";
+import TodoCounter from "../TodoCounter";
+import TodoList from "../TodoList";
+import TodoItem from "../TodoItem";
+import TodoSearch from "../TodoSearch";
+import CreateTodoButton from "../CreateTodoButton";
 
 function AppPrincipal({
-    totalTodos,
-    completados,
-    stateSearch,
-    setStateSearch, 
-    filTodos, 
-    completeTodos, 
-    eliminarTodos 
-}){
-
-return(
+  loading,
+  error,
+  totalTodos,
+  completados,
+  stateSearch,
+  setStateSearch,
+  filTodos,
+  completeTodos,
+  eliminarTodos,
+}) {
+  return (
     <React.Fragment>
-    <TodoCounter 
-    total = {totalTodos}
-    completed = {completados}
-    />
-      
-    <TodoSearch 
-      stateSearch = {stateSearch}
-      setStateSearch = {setStateSearch}
-    />
+      <TodoCounter total={totalTodos} completed={completados} />
 
-    <TodoList>
-      {filTodos.map(todo =>(
-        <TodoItem 
-          key={todo.text} 
-          text = {todo.text} 
-          completed = {todo.completed}
-          tachar = {() => completeTodos(todo.text)}
-          eliminar = {() => eliminarTodos(todo.text)}
-        />
-        )) }
+      <TodoSearch stateSearch={stateSearch} setStateSearch={setStateSearch} />
 
-    </TodoList>
-   <CreateTodoButton/>
-      
-  </React.Fragment>
+      <TodoList>
+        {loading && <p>Procesando por favor espere</p>}
+        {error && <p>Hubo un error</p>}
+        {!loading && !filTodos.length && <p>---Ingresa tu primer tarea---</p>}
 
-);
-
+        {filTodos.map((todo) => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            tachar={() => completeTodos(todo.text)}
+            eliminar={() => eliminarTodos(todo.text)}
+          />
+        ))}
+      </TodoList>
+      <CreateTodoButton />
+    </React.Fragment>
+  );
 }
 
 export default AppPrincipal;
--- src/App/index.js
+++ src/App/index.js
@@ -1,87 +1,99 @@
-import React, { useState } from 'react';
-import AppPrincipal from './AppPrincipal';
+import React, { useState } from "react";
+import { set } from "react-hook-form";
+import AppPrincipal from "./AppPrincipal";
 
 //import './App.css';
 
-const todos =[
-  {text : 'Practicar guitarra', completed: false},
-  {text : 'Hace ejercicio', completed: false},
-  {text : 'Estudiar ingles', completed: false},
-  {text : 'Comer saludable', completed: false},
-  {text : 'No comer azucar jajaj', completed: false}
-  
-]
+const todos = [
+  { text: "Practicar guitarra", completed: false },
+  { text: "Hace ejercicio", completed: false },
+  { text: "Estudiar ingles", completed: false },
+  { text: "Comer saludable", completed: false },
+  { text: "No comer azucar jajaj", completed: false },
+];
 
 function App() {
+  const useItems = (newItems, todoInicial) => {
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [items, setItem] = React.useState(todoInicial);
 
+    React.useEffect(() => {
+      setTimeout(() => {
+  try{const itemStorage = localStorage.getItem(newItems);
+        let auxStorage = todoInicial;
 
-  const useItems = (newItems, todoInicial)=>{
+        if (!itemStorage) {
+          localStorage.setItem(newItems, JSON.stringify(todoInicial));
+          auxStorage = todoInicial;
+        } else {
+          auxStorage = JSON.parse(itemStorage);
+        }
 
-  const itemStorage = localStorage.getItem(newItems);
-  let auxStorage = todoInicial;
+        setItem(auxStorage);
+        setLoading(false); 
+      } catch (error){
+        setError(error);
+      }
 
-  if (!itemStorage){
-    localStorage.setItem(newItems, JSON.stringify(todoInicial));
-    auxStorage = todoInicial;
-  } else{
-    auxStorage = JSON.parse(itemStorage);  
-  }
+      }, 2000);
 
-  const [items, setItem] = React.useState(auxStorage);
+    });
 
-  const saveTodos = (actTodos)=> {
-    localStorage.setItem(newItems, JSON.stringify(actTodos))
-    setItem(actTodos);
-  }
-  
-  return [
-    items,
-    saveTodos
-  ]
-}
-  
-  const [todoss, saveTodos] = useItems('ToDo_V1', []);
+    const saveTodos = (actTodos) => {
+      try {localStorage.setItem(newItems, JSON.stringify(actTodos));
+      setItem(actTodos);} catch(error){
+        setError(error);
+      }
+    };
+
+    return {items, saveTodos, loading, error};
+  };
+
+  const {items: todoss,
+         saveTodos: saveTodos,
+         loading,
+         error } = useItems("ToDo_V1", []);
   const [stateSearch, setStateSearch] = React.useState("");
-  const completados  = todoss.filter(todo => !!todo.completed).length;
-  const totalTodos =  todoss.length;
+  const completados = todoss.filter((todo) => !!todo.completed).length;
+  const totalTodos = todoss.length;
 
   let filTodos = [];
 
-  if (!stateSearch.length >= 1){
+  if (!stateSearch.length >= 1) {
     filTodos = todoss;
   } else {
-    filTodos = todoss.filter (todo =>{
-      const todoText = todo.text.toLowerCase()
+    filTodos = todoss.filter((todo) => {
+      const todoText = todo.text.toLowerCase();
       const searText = stateSearch.toLowerCase();
       return todoText.includes(searText);
-    })
+    });
   }
 
-    const completeTodos = (text) => {
-      const auxText = todoss.findIndex((todo) => todo.text === text );
-      const newTodos  = [...todoss];
-      newTodos[auxText].completed = true;
-      saveTodos(newTodos); 
+  const completeTodos = (text) => {
+    const auxText = todoss.findIndex((todo) => todo.text === text);
+    const newTodos = [...todoss];
+    newTodos[auxText].completed = true;
+    saveTodos(newTodos);
+  };
 
-    };
-
-    const eliminarTodos = (text) => {
-      const auxText = todoss.findIndex((todo) => todo.text === text );
-      const newTodos  = [...todoss];
-      newTodos.splice(auxText, 1);
-      saveTodos(newTodos);
-    }
-    
+  const eliminarTodos = (text) => {
+    const auxText = todoss.findIndex((todo) => todo.text === text);
+    const newTodos = [...todoss];
+    newTodos.splice(auxText, 1);
+    saveTodos(newTodos);
+  };
 
   return (
     <AppPrincipal
-    totalTodos = {totalTodos}
-    completados = {completados}
-    stateSearch ={stateSearch}
-    setStateSearch = {setStateSearch}
-    filTodos = {filTodos}
-    completeTodos = {completeTodos}
-    eliminarTodos = {eliminarTodos}
+      loading={loading}
+      totalTodos={totalTodos}
+      completados={completados}
+      stateSearch={stateSearch}
+      setStateSearch={setStateSearch}
+      filTodos={filTodos}
+      completeTodos={completeTodos}
+      eliminarTodos={eliminarTodos}
     />
   );
 }
